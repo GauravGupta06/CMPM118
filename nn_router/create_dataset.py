e82f00e32f827763bf5a92f0f634ec355315b9ad
Q-bh <kevinknguyen98@yahoo.com>
2025-11-07 15:36:19 -0800

    Add code to create CSV labels for ANN router

--- nn_router/create_dataset.py
+++ nn_router/create_dataset.py
@@ -18,9 +18,19 @@ from LoadDataset import load_dataset
 device = "cuda" if torch.cuda.is_available() else 'cpu'
 print(f"Using device: {device}")
 
+# Dataset parameters
 width = 32
 height = 32
 n_frames = 32
+
+# Model hyperparameters
+w_large = 32
+h_large = 32
+n_frames_large = 32
+
+w_small = 32
+h_small = 32
+n_frames_small = 32
 
 
 cached_train, cached_test, num_classes = load_dataset(
@@ -34,10 +44,10 @@ cached_train, cached_test, num_classes = load_dataset(
 
 # -- LOAD DENSE SNN --
 dense_model = SNNModel(
-    w=width,
-    h=height,
-    n_frames=n_frames,
-    beta= 0.6,
+    w=w_large,
+    h=h_large,
+    n_frames=n_frames_large,
+    beta= 0.8,
     spike_lam= 0,
     slope= 25,
     model_type="dense",
@@ -51,13 +61,13 @@ print("Dense model loaded successfully.")
 
 # -- LOAD SPARSE SNN --
 sparse_model = SNNModel(
-    w=width,
-    h=height,
-    n_frames=n_frames,
-    beta= 0.6,
-    spike_lam= 0,
+    w=w_small,
+    h=h_small,
+    n_frames=n_frames_small,
+    beta=0.4,
+    spike_lam= 1e-7,
     slope= 25,
-    model_type="dense",
+    model_type="sparse",
     device=device
 )
 
@@ -71,8 +81,9 @@ print("Sparse model loaded successfully.")
 cache_root = f"../data/dvsgesture/{width}x{height}_T{n_frames}"
 
 # -- WRITE DATA INTO data_train.csv --
-cached_train = tonic.DiskCachedDataset(None, cache_path=f"{cache_root}/train")
 """
+cached_train = tonic.DiskCachedDataset(None, cache_path=f"{cache_root}/train")
+
 print("Creating data_train.csv...")
 
 with open("data_train.csv", "w", newline="") as file:
@@ -109,63 +120,124 @@ print("data_train.csv has been created")
 
 
 # -- WRITE DATA INTO data_test.csv (it is the same process) --
-cached_test = tonic.DiskCachedDataset(None, cache_path=f"{cache_root}/test")
 """
+cached_test = tonic.DiskCachedDataset(None, cache_path=f"{cache_root}/test")
+
 print("Creating data_test.csv...")
+
 with open("data_test.csv", "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["lzc", "entropy", "std", "spike_count"])
-with open("data_test.csv", "a", newline="") as file:
-    writer = csv.writer(file)
-    for frames, label in cached_test:
-        flat_seq = (frames > 0).astype(int).flatten()
-        lzc = lempel_ziv_complexity(''.join(map(str, flat_seq.tolist())))
-        p = np.mean(flat_seq)
-        if p in [0, 1]:
-            entropy = 0
-        else:
-            entropy = -(p * np.log2(p) + (1 - p) * np.log2(1 - p))
-        std = np.std(flat_seq)
-        spike_count = np.sum(flat_seq)
-        writer.writerow([lzc, entropy, std, spike_count])
-print("data_test.csv has been created")
-"""
-
-
-
-
-def forward_pass(net, data):
-    spk_rec = []
-    snn.utils.reset(net)
-    with torch.no_grad():
-        for t in range(data.size(0)):          # data: [T, 2, H, W]
-            x = data[t].unsqueeze(0).to(device) # -> [1, 2, H, W]
-            spk_out, _ = net(x)
-            spk_rec.append(spk_out)             # [1, 11]
-    return torch.stack(spk_rec)  
-
-
-def predict_sample(net, frames):
-    frames = torch.tensor(frames, dtype=torch.float)  # [T, 2, H, W]
-    spk_rec = forward_pass(net, frames)
-    counts = spk_rec.sum(0)            # [1, 11]
-    return counts.argmax(1).item()
-
-# -- WRITE DATA INTO data_nn.csv --
-print("Creating data_snn.csv...")
-"""
-with open("data_snn.csv", "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(["use_small", "use_large"])
 
 with open("data_train.csv", "a", newline="") as file:
     writer = csv.writer(file)
 
     for frames, label in cached_train:
-        pred_l = predict_sample(net_l, frames)
-        pred_s = predict_sample(net_s, frames)
+        flat_seq = (frames > 0).astype(int).flatten()
+        
+        # LZC
+        lzc = lempel_ziv_complexity(''.join(map(str, flat_seq.tolist())))
 
-        writer.writerow([None, None])
-        break
+        # Entropy
+        p = np.mean(flat_seq)
+        if p in [0, 1]:
+            entropy = 0
+        else:
+            entropy = -(p * np.log2(p) + (1 - p) * np.log2(1 - p))
+
+        # Standard Deviation
+        std = np.std(flat_seq)
+
+        # Spike Count
+        spike_count = np.sum(flat_seq)
+
+        writer.writerow([lzc, entropy, std, spike_count])
+        
+print("data_test.csv has been created")
 """
-print("data_snn.csv has been created")
+
+# -----------------------------------------------------------------------------------------------------------------------------
+
+# -- CREATING SNN CSVs --
+"""
+Truth Table:
++----------------+---------------+------------+-----------+
+| correct_sparse | correct_dense | use_sparse | use_dense |
++----------------+---------------+------------+-----------+
+|            1.0 |           1.0 | True       | False     |
+|            1.0 |           0.0 | True       | False     |
+|            0.0 |           1.0 | False      | True      |
+|            0.0 |           0.0 | False      | True      |
++----------------+---------------+------------+-----------+
+"""
+
+active_cores = 0
+
+
+# -- WRITE DATA INTO data_train_snn.csv --
+# Note to self: when I get back, I probably wanna change shuffle to = False
+"""
+train_loader = torch.utils.data.DataLoader(cached_train, batch_size=1, shuffle=True, num_workers = active_cores, drop_last=True, 
+                                           collate_fn=tonic.collation.PadTensors(batch_first=False))
+
+print("Creating data_train_snn.csv...")
+
+with open("data_train_snn.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["sparse_result", "dense_results", "use_sparse", "use_dense"])
+
+with open("data_train_snn.csv", "a", newline="") as file:
+    writer = csv.writer(file)
+
+    correct_dense, correct_sparse = 0, 0
+    for frames, label in train_loader:
+        pred_dense, _ = dense_model.forward_pass(frames)
+        pred_sparse, _ = sparse_model.forward_pass(frames)
+
+        correct_dense = SF.accuracy_rate(pred_dense, label) * frames.shape[1]
+        correct_sparse = SF.accuracy_rate(pred_sparse, label) * frames.shape[1]
+
+        if correct_sparse:
+            use_sparse = True
+            use_dense = False
+        else:
+            use_sparse = False
+            use_dense = True
+
+        writer.writerow([correct_sparse, correct_dense, use_sparse, use_dense])
+
+print("data_train_snn.csv has been created")
+"""
+
+# -- WRITE DATA INTO data_test_snn.csv --
+# Note to self: when I get back, I probably wanna change shuffle to = False
+test_loader = torch.utils.data.DataLoader(cached_test, batch_size=1, shuffle=True, num_workers = active_cores, drop_last=True, 
+                                          collate_fn=tonic.collation.PadTensors(batch_first=False))
+
+print("Creating data_test_snn.csv...")
+
+with open("data_test_snn.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["sparse_result", "dense_results", "use_sparse", "use_dense"])
+
+with open("data_test_snn.csv", "a", newline="") as file:
+    writer = csv.writer(file)
+
+    correct_dense, correct_sparse = 0, 0
+    for frames, label in test_loader:
+        pred_dense, _ = dense_model.forward_pass(frames)
+        pred_sparse, _ = sparse_model.forward_pass(frames)
+
+        correct_dense = SF.accuracy_rate(pred_dense, label) * frames.shape[1]
+        correct_sparse = SF.accuracy_rate(pred_sparse, label) * frames.shape[1]
+
+        if correct_sparse:
+            use_sparse = True
+            use_dense = False
+        else:
+            use_sparse = False
+            use_dense = True
+
+        writer.writerow([correct_sparse, correct_dense, use_sparse, use_dense])
+
+print("data_test_snn.csv has been created")
